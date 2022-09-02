a43a313d543e68cdb36f764b0bb4da2c5ca6c20a
daiyunzhou <daiyunz@chanjet.com>
2022-09-02 09:40:28 +0800

    add test code

--- src/index.js
+++ src/index.js
@@ -25,18 +25,86 @@ class Canvas {
   /**
    * 填充数据
    */
-  fillStyle() {
-    this.context.fillStyle = "#454034";
-    this.context.fillRect(10, 10, 150, 100);
+  fillStyle(opt) {
+    console.log(opt);
+    this.context.fillStyle = `rgba(0, 0, 200, ${opt})`;
+    this.context.clearRect(0, 0, 150, 100);
+    this.context.strokeRect(10, 10, 150, 100);
+    this.context.fillRect(0, 0, 150, 100);
   }
   /**
    * 初始化
    */
   init() {
-    this.fillStyle();
+    let opt = 1;
+    let isAdd = false;
+    setInterval(() => {
+      if (isAdd) {
+        opt = opt + 0.1;
+        if (opt >= 1) {
+          isAdd = false;
+        }
+      } else {
+        opt = opt - 0.1;
+        if (opt <= 0) {
+          isAdd = true;
+        }
+      }
+      opt = Number(Number(opt).toFixed(2));
+      this.fillStyle(opt);
+    }, 500);
+  }
+  /**
+   * 三角
+   */
+  move() {
+    const ctx = this.context;
+    ctx.beginPath();
+    ctx.arc(75, 75, 50, 0, Math.PI * 0.2, true); // 绘制
+    ctx.moveTo(110, 75);
+    ctx.arc(75, 75, 35, 0, Math.PI, false); // 口 (顺时针)
+    ctx.moveTo(65, 65);
+    ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // 左眼
+    ctx.moveTo(95, 65);
+    ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // 右眼
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(225, 125);
+    ctx.lineTo(225, 45);
+    ctx.lineTo(145, 125);
+    ctx.closePath();
+    ctx.stroke();
+
+    // ctx.beginPath();
+    // ctx.moveTo(25, 25);
+    // ctx.lineTo(105, 25);
+    // ctx.lineTo(25, 105);
+    // ctx.fill();
+  }
+  /**
+   * 获取鼠标坐标
+   * @param {*} ev
+   * @returns
+   */
+  getEventPosition(ev) {
+    return { x: ev.offsetX, y: ev.offsetY };
+  }
+  /**
+   * 鼠标事件
+   */
+  onClick(){
+    this.canvas.onclick = (e) =>{
+      const point = this.getEventPosition(e);
+      const result = this.context.isPointInPath(point.x,point.y);
+      console.log(e,point,result);
+
+    }
   }
 }
 window.onload = () => {
   const myCanvas = new Canvas(document.getElementById("canvas"), {});
-  myCanvas.init();
+  // myCanvas.init();
+  myCanvas.move();
+  myCanvas.onClick();
 };
